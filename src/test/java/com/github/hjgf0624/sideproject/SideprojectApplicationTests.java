d572f81208b9c3603830496275021365fbd4b453
hjgf0624 <39186964+hjgf0624@users.noreply.github.com>
2025-02-16 23:44:55 +0900

    first commit

---
+++ src/test/java/com/github/hjgf0624/sideproject/SideprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hjgf0624.sideproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SideprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
